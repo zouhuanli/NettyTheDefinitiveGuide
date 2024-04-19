64914b32498db6364bc976a6d868243a927251bb
zouhl <zouhl0924@gmail.com>
2024-04-19 22:29:05 +0800

    ch8:protobuf协议

---
+++ bookSourceCode/src/main/java/io/github/zouhuanli/others/SemaphoreTest.java
@@ -0,0 +1,27 @@
+package io.github.zouhuanli.ch6;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+    public static void main(String[] args) {
+        // 初始化 Semaphore，指定容量为 5
+        Semaphore semaphore = new Semaphore(5);
+
+        // 调用 release 方法释放 6 个许可证
+        for (int i = 0; i < 6; i++) {
+            semaphore.release();
+            System.out.println("Released a permit, available permits: " + semaphore.availablePermits());
+        }
+
+        for (int i=0; i<11; i++){
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Acquired a permit, available permits: " + semaphore.availablePermits());
+        }
+    }
+}
+
+
